3ecaa5fcd3c9b7fe56c97a94ad5e75fc219715ef
hazamaswag <hamzasayyid@gmail.com>
2020-05-13 17:43:38 -0700

    changed read me

--- CS182-Spring2020-Vision-Project/test_submission.py
+++ CS182-Spring2020-Vision-Project/test_submission.py
@@ -4,27 +4,27 @@ import PIL
 import numpy as np
 import tensorflow as tf
 import tensorflow.keras.preprocessing.image as image
-
+# from fool_image import fool_image
 
 def main():
 
     # Load the model
-    model = tf.keras.models.load_model('model.h5')
+    model = tf.keras.models.load_model('./models/incep3.h5')
+    # model = VGG16(weights='imagenet', include_top=False)
 
     # Load the classes
     data_dir = pathlib.Path('./data/tiny-imagenet-200/train/')
     CLASSES = sorted([item.name for item in data_dir.glob('*')])
 
     # Loop through the CSV file and make a prediction for each line
-    with open('eval_classified.csv', 'w') as eval_output_file:  # Open the evaluation CSV file for writing
+    with open('eval_classified_val.csv', 'w') as eval_output_file:  # Open the evaluation CSV file for writing
         for line in pathlib.Path(sys.argv[1]).open():  # Open the input CSV file for reading
-            image_id, image_path, image_height, image_width, image_channels = line.strip().split(
-                ',')  # Extract CSV info
+            image_id, image_path, image_height, image_width, image_channels = line.strip().split(',')  # Extract CSV info
 
             print(image_id, image_path, image_height, image_width, image_channels)
 
             # Preprocess our data
-            image_data = image.img_to_array(image.load_img(image_path, target_size=(64, 64)))
+            image_data = image.img_to_array(image.load_img(image_path, target_size=(128, 128)))
             preprocessed_image_data = image_data / 255.0
 
             # Generate a prediction
@@ -32,6 +32,7 @@ def main():
             prediction = int(np.argmax(prediction_probs.reshape(-1)))
 
             # Write the prediction to the output file
+            # print(prediction)
             eval_output_file.write('{},{}\n'.format(image_id, CLASSES[prediction]))
 
 
--- CS182-Spring2020-Vision-Project/train_sample.py
+++ CS182-Spring2020-Vision-Project/train_sample.py
@@ -10,22 +10,25 @@ import numpy as np
 import tensorflow as tf
 import keras
 
+from keras.utils.vis_utils import plot_model
+
 from PIL import Image
 from keras.applications.vgg16 import VGG16
-from keras.applications.vgg19 import VGG19
+from keras.applications.vgg16 import VGG16
+from keras.applications.resnet import ResNet50
 from keras.applications.inception_v3 import InceptionV3
 from keras.applications.mobilenet import MobileNet
 from keras.applications.densenet import DenseNet121, DenseNet169, DenseNet201
+from keras.applications.nasnet import NASNetLarge
 
 from keras.preprocessing import image
 from keras.models import Model, Sequential
-from keras.layers import Input, Flatten, Dense, Conv2D, MaxPool2D
+from keras.layers import Input, Flatten, Dense, Conv2D, MaxPool2D, GlobalAveragePooling2D, Dropout
 from utils.log import log, TimeHistory, cparams
-# from fool_image import fool_image
 
 def main():
     # Create a tensorflow dataset
-    data_dir = pathlib.Path('./data/tiny-imagenet-200/new_train/')
+    data_dir = pathlib.Path('./data/tiny-imagenet-200/b_train/')
     val_data_dir = pathlib.Path('./data/tiny-imagenet-200/validation/')
 
     image_count = len(list(data_dir.glob('**/*.JPEG')))
@@ -35,73 +38,54 @@ def main():
     print('Discovered {} images'.format(image_count))
 
     # Simple image preprocessing:  Scale images to [0, 1] float32
-    image_generator = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1. / 255)
-    train_gen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1. / 255, zoom_range=0.2, horizontal_flip=True, rotation_range=30)
-    train_gen2 = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1. / 255)
+    image_gen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1. / 255)
 
 
     # Create the training data generator
     BATCH_SIZE = 32
-    IMG_HEIGHT = 64
-    IMG_WIDTH = 64
+    IMG_HEIGHT = 128
+    IMG_WIDTH = 128
     STEPS_PER_EPOCH = np.ceil(image_count / BATCH_SIZE)
-    train_data_gen = train_gen.flow_from_directory(directory=str(data_dir),
+    train_data_gen = image_gen.flow_from_directory(directory=str(data_dir),
                                                          batch_size=BATCH_SIZE,
                                                          shuffle=True,
                                                          target_size=(IMG_HEIGHT, IMG_WIDTH),
                                                          classes=list(CLASS_NAMES),
+                                                         interpolation='bicubic',
                                                          seed=7)
 
-    train_data_gen2 = train_gen2.flow_from_directory(directory=str(data_dir),
+
+    val_data_gen = image_gen.flow_from_directory(directory=str(val_data_dir),
                                                          batch_size=BATCH_SIZE,
                                                          shuffle=True,
                                                          target_size=(IMG_HEIGHT, IMG_WIDTH),
-                                                         classes=list(CLASS_NAMES),
-                                                         seed = 7)
-
-    # fool_image(train_data_gen)
-    # fool_image(train_data_gen2)
-
-
-    val_data_gen = image_generator.flow_from_directory(directory=str(val_data_dir),
-                                                         batch_size=BATCH_SIZE,
-                                                         shuffle=True,
-                                                         target_size=(IMG_HEIGHT, IMG_WIDTH),
+                                                         interpolation='bicubic',
                                                          classes=list(val_names))
 
-    # Create a simple model
-    # model = tf.keras.Sequential([
-    #     tf.keras.layers.Reshape((64*64*3,), input_shape=(64, 64, 3)),
-    #     tf.keras.layers.Dense(128, activation='relu'),
-    #     tf.keras.layers.Dense(len(CLASS_NAMES), activation='softmax'),
-    # ])
 
-    input = Input(shape=(64,64,3),name = 'image_input')
+    input = Input(shape=(IMG_HEIGHT,IMG_WIDTH,3), name = 'image_input')
 
-    image_model = DenseNet169(weights='imagenet', include_top=False)
-    # update weights
-    out_model = image_model(input)
-
-    # for layer in mobile_net.layers[:-5]:
-    #     layer.trainable = False
-    # for layer in mobile_net.layers:
-    #     print(layer, layer.trainable)
+    image_model = InceptionV3(weights='imagenet', include_top=False)
+    image_model.summary()
+    image_model = image_model(input)
 
 
-
-    x = Flatten(name='flatten')(out_model)
+    x = Flatten(name='flatten')(image_model)
     x = Dense(4096, activation='relu', name='fc1')(x)
     x = Dense(4096, activation='relu', name='fc2')(x)
     x = Dense(200, activation='sigmoid', name='predictions')(x)
 
     model = Model(input=input, output=x)
+    plot_model(model, to_file='incep3.png', show_shapes=True)
+
     model.summary()
 
     lr = 1e-6
-    epochs = 10
-    model_name = 'dense169_5f'
+    epochs = 15
+    model_name = 'incep3'
 
-    # tf.keras.optimizers.adam(...)
+
+
     model.compile(optimizer=keras.optimizers.Adam(lr=lr),
                   loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
                   metrics=['accuracy', 'top_k_categorical_accuracy'])
